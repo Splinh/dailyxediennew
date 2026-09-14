cca0fa66aef6fdab534c6d7be1ff51ca0c5aa072
Splinh <splworks.info@gmail.com>
2026-09-14 10:59:36 +0700

    chore: cleanup probe scripts and restore secure debug settings

--- config/environments/production.php
+++ config/environments/production.php
@@ -11,10 +11,10 @@
 
 use Roots\WPConfig\Config;
 
-/** Ensure debug is temporarily enabled for diagnostic */
-Config::define( 'WP_DEBUG', true );
-Config::define( 'WP_DEBUG_DISPLAY', true );
-Config::define( 'WP_DEBUG_LOG', true );
+/** Ensure debug is disabled in production */
+Config::define( 'WP_DEBUG', false );
+Config::define( 'WP_DEBUG_DISPLAY', false );
+Config::define( 'WP_DEBUG_LOG', false );
 Config::define( 'SCRIPT_DEBUG', false );
 Config::define( 'SAVEQUERIES', false );
 
